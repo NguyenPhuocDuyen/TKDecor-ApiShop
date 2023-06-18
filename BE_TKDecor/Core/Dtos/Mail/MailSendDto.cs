78191c012d46e77d8f25f3ecde8f8c872694198a
DELL_5402\duyen <DuyenNP7901@gmail.com>
2023-06-18 19:55:32 +0700

    feature contact page

---
+++ BE_TKDecor/Core/Dtos/Mail/MailSendDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BE_TKDecor.Core.Dtos.Mail
+{
+    public class MailSendDto
+    {
+        public string Name { get; set; } = null!;
+        public string MailSender { get; set; } = null!;
+        public string Content { get; set; } = null!;
+    }
+}
