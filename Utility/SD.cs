a6516e4816990881383f17fce0fdcd249d890a9e
DuyenNP <90950103+NguyenPhuocDuyen@users.noreply.github.com>
2023-08-02 08:45:36 +0700

    english status

--- Utility/SD.cs
+++ Utility/SD.cs
@@ -11,33 +11,62 @@ namespace Utility
         // signal R notification
         public const string NewNotification = "NewNotification";
 
+        //// role
+        //public const string RoleAdmin = "Quản trị viên";
+        //public const string RoleCustomer = "Khách hàng";
+
+        //// coupon type
+        //public const string CouponByPercent = "Giảm theo phần trăm";
+        //public const string CouponByValue = "Giảm theo giá tiền";
+
+        //// gender
+        //public const string GenderMale = "Nam";
+        //public const string GenderFemale = "Nữ";
+        //public const string GenderOther = "Khác";
+
+        //// interaction
+        //public const string InteractionLike = "Thích";
+        //public const string InteractionDislike = "Không thích";
+        //public const string InteractionNormal = "Bình thường";
+
+        //// order
+        //public const string OrderOrdered = "Đã đặt đơn hàng";
+        //public const string OrderDelivering = "Đang giao đơn hàng";
+        //public const string OrderReceived = "Đã nhận đơn hàng";
+        //public const string OrderCanceled = "Đã huỷ đơn hàng";
+
+        //// report status
+        //public const string ReportPending = "Chưa xử lý";
+        //public const string ReportAccept = "Đã chấp nhận";
+        //public const string ReportReject = "Đã từ chối";
+
         // role
-        public const string RoleAdmin = "Quản trị viên";
-        public const string RoleCustomer = "Khách hàng";
+        public const string RoleAdmin = "Admin";
+        public const string RoleCustomer = "Customer";
 
         // coupon type
-        public const string CouponByPercent = "Giảm theo phần trăm";
-        public const string CouponByValue = "Giảm theo giá tiền";
+        public const string CouponByPercent = "ByPercent";
+        public const string CouponByValue = "ByValue";
 
         // gender
-        public const string GenderMale = "Nam";
-        public const string GenderFemale = "Nữ";
-        public const string GenderOther = "Khác";
+        public const string GenderMale = "Male";
+        public const string GenderFemale = "Female";
+        public const string GenderOther = "Other";
 
         // interaction
-        public const string InteractionLike = "Thích";
-        public const string InteractionDislike = "Không thích";
-        public const string InteractionNormal = "Bình thường";
+        public const string InteractionLike = "Like";
+        public const string InteractionDislike = "Dislike";
+        public const string InteractionNormal = "Normal";
 
         // order
-        public const string OrderOrdered = "Đã đặt đơn hàng";
-        public const string OrderDelivering = "Đang giao đơn hàng";
-        public const string OrderReceived = "Đã nhận đơn hàng";
-        public const string OrderCanceled = "Đã huỷ đơn hàng";
+        public const string OrderOrdered = "Ordered";
+        public const string OrderDelivering = "Delivering";
+        public const string OrderReceived = "Received";
+        public const string OrderCanceled = "Canceled";
 
         // report status
-        public const string ReportPending = "Chưa xử lý";
-        public const string ReportAccept = "Đã chấp nhận";
-        public const string ReportReject = "Đã từ chối";
+        public const string ReportPending = "Pending";
+        public const string ReportAccept = "Accept";
+        public const string ReportReject = "Reject";
     }
 }
